bd4766616f4dfa8af9d396b8ee3e6009a39a673c
Patrick Stockton <patrick@codejnki.com>
2014-02-22 14:52:12 -0500

    Initial commit for a reimplemented macro as an Outlook Addin

--- AutoMailSave/ThisAddIn.cs
+++ AutoMailSave/ThisAddIn.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,28 +9,57 @@ using Office = Microsoft.Office.Core;
 
 namespace AutoMailSave
 {
-    public partial class ThisAddIn
-    {
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
-        {
-        }
+	public partial class ThisAddIn
+	{
+		private void ThisAddIn_Startup(object sender, System.EventArgs e)
+		{
+			// Create an event handler for when items are sent
+			Application.ItemSend += new Outlook.ApplicationEvents_11_ItemSendEventHandler(SaveEmail);
+		}
 
-        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
-        {
-        }
 
-        #region VSTO generated code
+		private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+		{
+		}
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InternalStartup()
-        {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
-        }
-        
-        #endregion
-    }
+		private void SaveEmail(object Item, ref bool Cancel)
+		{
+			var message = Item as Outlook.MailItem;
+
+			string fileName = FileName(message.Recipients[1]);
+			string savePath = PathName();
+
+			message.SaveAs(savePath + fileName, Outlook.OlSaveAsType.olMSG);
+		}
+
+		private string PathName()
+		{
+			string pathName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\MailSave\" + DateTime.Now.ToString("yyyy-MM-dd");
+
+			Directory.CreateDirectory(pathName);
+
+			return pathName;
+		}
+
+		private string FileName(Outlook.Recipient recipient)
+		{
+			string fileName = @"\" + recipient.Address + "_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".msg";
+
+			return fileName;
+		}
+
+		#region VSTO generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InternalStartup()
+		{
+				this.Startup += new System.EventHandler(ThisAddIn_Startup);
+				this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+		}
+				
+		#endregion
+	}
 }
